4b303da4b9066e0f91177ddde5aab9c0521d8400
poly000 <1348292515@qq.com>
2022-08-08 17:33:07 +0800

    code refactor

--- src/main.rs
+++ src/main.rs
@@ -23,50 +23,7 @@ async fn main() {
     let handler = Update::filter_inline_query().branch(dptree::endpoint(
         |query: InlineQuery, bot: AutoSend<Bot>| async move {
             let mut results = vec![];
-            let case;
-
-            match &*query.query {
-                "" => {
-                    case = Case::Random;
-                    let text = TIPS[rand_index(0..TIPS.len()).await];
-                    let content_text = InputMessageContentText::new(text);
-                    let content = InputMessageContent::Text(content_text);
-                    let random_result = InlineQueryResult::Article(InlineQueryResultArticle::new(
-                        "0", text, content,
-                    ));
-
-                    results.push(random_result);
-                }
-                "*" => {
-                    case = Case::List;
-                    let text = "https://github.com/poly000/phigbot/blob/main/src/constants/mod.rs";
-                    let content = InputMessageContent::Text(InputMessageContentText::new(text));
-                    let result = InlineQueryResult::Article(InlineQueryResultArticle::new(
-                        "0",
-                        "全部tips见mod.rs",
-                        content,
-                    ));
-                    results.push(result);
-                }
-                _ => {
-                    case = Case::Search;
-                    let downcase_key = query.query.to_lowercase();
-                    for (&text, i) in TIPS
-                        .iter()
-                        .filter(|s| s.to_lowercase().contains(&downcase_key))
-                        .zip(0..)
-                    {
-                        let content = InputMessageContent::Text(InputMessageContentText::new(text));
-                        let result = InlineQueryResult::Article(InlineQueryResultArticle::new(
-                            i.to_string(),
-                            text,
-                            content,
-                        ));
-                        results.push(result);
-                    }
-                }
-            }
-
+            let case = get_results(&mut results, &query);
             let cache_time = match case {
                 // should be the link to `mod.rs` forever
                 Case::List => u32::MAX,
@@ -94,6 +51,49 @@ async fn main() {
     Dispatcher::builder(bot, handler).build().dispatch().await;
 }
 
-async fn rand_index(range: Range<usize>) -> usize {
+fn rand_index(range: Range<usize>) -> usize {
     thread_rng().gen_range(range)
+}
+
+fn get_results(results: &mut Vec<InlineQueryResult>, query: &InlineQuery) -> Case {
+    match &*query.query {
+        "" => {
+            let text = TIPS[rand_index(0..TIPS.len())];
+            let content_text = InputMessageContentText::new(text);
+            let content = InputMessageContent::Text(content_text);
+            let random_result =
+                InlineQueryResult::Article(InlineQueryResultArticle::new("0", text, content));
+
+            results.push(random_result);
+            Case::Random
+        }
+        "*" => {
+            let text = "https://github.com/poly000/phigbot/blob/main/src/constants/mod.rs";
+            let content = InputMessageContent::Text(InputMessageContentText::new(text));
+            let result = InlineQueryResult::Article(InlineQueryResultArticle::new(
+                "0",
+                "全部tips见mod.rs",
+                content,
+            ));
+            results.push(result);
+            Case::List
+        }
+        _ => {
+            let downcase_key = query.query.to_lowercase();
+            for (&text, i) in TIPS
+                .iter()
+                .filter(|s| s.to_lowercase().contains(&downcase_key))
+                .zip(0..)
+            {
+                let content = InputMessageContent::Text(InputMessageContentText::new(text));
+                let result = InlineQueryResult::Article(InlineQueryResultArticle::new(
+                    i.to_string(),
+                    text,
+                    content,
+                ));
+                results.push(result);
+            }
+            Case::Search
+        }
+    }
 }
